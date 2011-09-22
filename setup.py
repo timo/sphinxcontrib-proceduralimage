3c9848dd0bd5fe0f41ad1d7db4fc48ba1c240ce6
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-09-22 23:34:10 +0200

    move help text into README

--- setup.py
+++ setup.py
@@ -1,20 +1,9 @@
 # -*- coding: utf-8 -*-
-'''
-This package adds the proceduralimage directive to Sphinx.
-
-.. _Sphinx: http://sphinx.pocoo.org/
-
-It allows a python code snippet to be supplied, that generates an image, that
-will be included in the documentation.
-
-It adds a single directive, `proceduralimage`. It takes a block of python
-code, executes the code and extracts the local variables `image_data` and
-`alt`. image_data will be saved to a .png in _images and embedded and
-the alt variable will be used as the images alt tag.
-'''
 
 from setuptools import setup
 
+with open("README") as readme:
+    long_description = readme
 
 requires = ['Sphinx>=1.0']
 
@@ -25,7 +14,7 @@ setup(
     author='Timo Paulssen',
     author_email='timonator@perpetuum-immobile.de',
     description='proceduralimage Sphinx extension',
-    long_description=__doc__,
+    long_description=long_description,
     url="https://github.com/timo/sphinxcontrib-proceduralimage",
     zip_safe=False,
     classifiers=[
